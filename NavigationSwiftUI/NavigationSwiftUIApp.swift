eae35b4065ce6a85da2c03d8ba963fcc6e5c2eec
Sajith Wijerathne <sajith.wijerathne@simplitude.se>
2024-05-06 00:20:50 +0200

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Sajith on 2024-05-06.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
