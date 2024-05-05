45d1ce18f04bfdb9fa39363e7b838051d77e1c05
ruk91 <sajithkasn@gmail.com>
2024-05-06 01:14:19 +0200

    🎉 First commit

--- NavigationSwiftUI/ContentView.swift
+++ NavigationSwiftUI/ContentView.swift
@@ -9,15 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "house")
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .foregroundColor(.blue)
+                    .aspectRatio(contentMode: .fit)
+                Text("Home screen")
+                    .font(.title)
+                    .padding()
+                
+                NavigationLink(destination: ProfileView()) {
+                    Text("Navigate to Profile")
+                        .frame(width: 200, height: 50, alignment: .center)
+                        .background(Color.pink)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .navigationTitle("Home")
         }
-        .padding()
     }
 }
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.blue)
+                .aspectRatio(contentMode: .fit)
+            Text("Profile screen")
+                .font(.title)
+                .padding()
+            
+            NavigationLink(destination: ContentView()) {
+                Text("Navigate to Home")
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .background(Color.pink)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .navigationTitle("Profile")
+    }
+}
+
 
 #Preview {
     ContentView()
